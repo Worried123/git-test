b526519c18e6d10d7b484e16b5e4c7aa0c2914a7
amir <amirmohseni261@gmail.com>
2023-08-12 03:14:52 +0330

    fixed some bugs and added an answer space

--- calculator/script.js
+++ calculator/script.js
@@ -1,32 +1,7 @@
 let num1 ='' ;
 let num2 = '';
 let operator;
-function add(a,b) {
-return a+b
-}
-function sub(a,b) {
-    return a-b
-}
-function multiply(a,b) {
-    return a*b
-}
-function devide(a,b) {
-    return a/b
-}
-function operate(num1,operator,num2) {
-    if (operator === '+') {
-    return add(num1,num2)    
-    }
-    else if (operator === '-') {
-       return sub(num1,num2)    
-        }
-        else if (operator === '*') {
-          return  multiply(num1,num2)    
-            }
-            else if (operator === '/') {
-              return  devide(num1,num2)    
-                }
-}
+
 const show =document.querySelector('.screen');
 const numbers = document.querySelectorAll('.numbers')
 const equal = document.querySelector('.equal')
@@ -34,6 +9,7 @@ const operat = document.querySelectorAll('.operators')
 const reset = document.querySelector('.ac')
 const percentage = document.querySelector('.tohundred')
 const negative =  document.querySelector('.negative')
+const answer = document.querySelector('.answer')
 numbers.forEach(number=>{
 number.addEventListener('click', e=>{
   show.textContent += number.textContent
@@ -48,6 +24,7 @@ number.addEventListener('click', e=>{
     num1 =operate(+num1,operator,+num2)
    
         }
+  answer.textContent = num1
   })
 })
  equal.addEventListener('click', e=> {
@@ -68,6 +45,7 @@ reset.addEventListener('click', e=>{
  num1 = ''
  num2 = ''
  operator = ''
+ answer.textContent = ''
 })
 percentage.addEventListener('click' , e=>{
 show.textContent = num1/100
@@ -81,6 +59,37 @@ equal.addEventListener('click' , e => {
   num2 = ''
 console.log(num1);
 })
+function add(a,b) {
+  return a+b
+  }
+  function sub(a,b) {
+      return a-b
+  }
+  function multiply(a,b) {
+      return a*b
+  }
+  function devide(a,b) {
+      return a/b
+  }
+  function operate(num1,operator,num2) {
+      if (operator === '+') {
+      return add(num1,num2)    
+      }
+      else if (operator === '-') {
+         return sub(num1,num2)    
+          }
+          else if (operator === '*') {
+            return  multiply(num1,num2)    
+              }
+              else if (operator === '/') {
+              if(num1 === 0 || num2 === 0){
+                num1 = ''
+                num2 = ''
+                show.textContent = 'eror'
+              }else{
+                return  devide(num1,num2)    
+              }}
+  }
 //write a code that when a button is pressed it show it on scereen
 //write a code that does the same thing for operators
 //add the data of pressed button to a arraay
